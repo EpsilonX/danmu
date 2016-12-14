4046134e03a6f30984daa651d348f12e97128556
柳萌宇 <jeffreyliu09@gmail.com>
2016-12-14 20:15:12 +0800

    update

--- js/jquery.barrager.js
+++ js/jquery.barrager.js
@@ -1,8 +1,3 @@
-/*!
- *@name     jquery.barrager.js
- *@author   yaseng@uauc.net
- *@url      https://github.com/yaseng/jquery.barrager.js
- */
 (function($) {
 	window_width = $(window).width() + 500
 	$.fn.barrager = function(barrage) {
@@ -18,28 +13,23 @@
 		var time = new Date().getTime();
 		var barrager_id = 'barrage_' + time;
 		var id = '#' + barrager_id;
-		var div_barrager = $("<div class='barrage' id='" + barrager_id + "'></div>").appendTo($(this));
+		var $rectDashed = $('<div class="rect-dashed"></div>')
+		var $rectSolid = $('<div class="rect-solid"></div>').append($rectDashed)
+		var div_barrager = $("<div class='barrage' id='" + barrager_id + "'></div>").append($rectSolid).appendTo($(this));
 		var window_height = $(window).height() - 100;
 		var bottom = (barrage.bottom == 0) ? Math.floor(Math.random() * window_height + 40) : barrage.bottom;
-		div_barrager.css({"bottom": bottom + "px"});
-		div_barrager_box = $("<div class='barrage_box cl'></div>").appendTo(div_barrager);
-		if(barrage.img){
-			div_barrager_box.append("<a class='portrait z' href='javascript:;'></a>");
-			var img = $("<img src='' >").appendTo(id + " .barrage_box .portrait");
-			img.attr('src', barrage.img);
-		}
+		div_barrager.css({'bottom': bottom + 'px'});
+		div_barrager_box = $("<div class='barrage_box cl'></div>").appendTo($rectDashed);
 		
 		div_barrager_box.append(" <div class='z p'></div>");
-		if(barrage.close){
-			div_barrager_box.append(" <div class='close z'></div>");
-		}
 		
 		var content = $(id + " .barrage_box .p").append('<span class="vt-midl">'+ barrage.info +'</span>')
-		content.click(function() {
-			barrage.callback(barrage.params)
-		})
-		
 		content.css('color', barrage.color);
+		if (barrage.isAd) {
+			content.click(function() {
+				barrage.callback(barrage.params)
+			})
+		}
 		
 		$(id).animate({'margin-right': window_width}, barrage.speed, 'linear', function() {
 			$(this).remove()
